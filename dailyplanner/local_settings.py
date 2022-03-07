915e20e2184427aa104210a937e756b25a2c9943
vahid <vahidrafiyi@yahoo.com>
2022-03-07 10:52:49 +0330

    delete local_setting.py and .gitignore

--- dailyplanner/local_settings.py
+++
@@ -1,5 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)%+kj27gjkurj)&ob+q-^2pq*!x__a^4yfz!djlgf$hsal-#8c'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
--- dailyplanner/settings.py
+++ dailyplanner/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-from dailyplanner.local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-)%+kj27gjkurj)&ob+q-^2pq*!x__a^4yfz!djlgf$hsal-#8c'
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
